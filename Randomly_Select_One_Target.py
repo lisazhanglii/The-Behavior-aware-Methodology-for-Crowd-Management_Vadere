940395f299c390518ebb29f37acefb1d1b1494d6
lisazhanglii <2010295336@qq.com>
2023-07-13 02:35:51 -0600

    set random target for everyone

--- Randomly_Select_One_Target.py
+++ Randomly_Select_One_Target.py
@@ -1,7 +1,8 @@
 import json
 import random
 
-
+random.seed(1)
+counter = [0, 0, 0, 0]
 id_list = []
 with open('pedestrian.json', encoding='utf-8') as a:
     data = json.load(a)
@@ -16,7 +17,13 @@ with open('pedestrian.json', encoding='utf-8') as a:
     data = json.load(a)
     x = data['dynamicElements']
     for y in range(0,len(x)):
+            target = random.randint(1, 4)
+            counter[target - 1] += 1
             data['dynamicElements'][int(y)]['targetIds'][0]= random.randint(1, 4)
+
 with open('pedestrian.json', 'w') as file:
     json.dump(data, file, indent=2)
     print("Finihsed randomly distributing the targets of all the persons!")
+
+for i in range(4):
+    print("target", i+1, "appears", counter[i],"times")
