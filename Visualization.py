52f1b09e91fc26da540d7bae4ec8922c80a0e4b0
lisazhanglii <2010295336@qq.com>
2023-07-15 14:42:23 -0600

    2a and 2b visualization

--- Visualization.py
+++ Visualization.py
@@ -3,13 +3,13 @@ import plotly.graph_objects as go
 animals=['elderly people average time', 'normal people average time','average time of all','max time of all','min time of all', 'target1 time', 'target2 time','target3 time','target4 time']
 
 fig = go.Figure(data=[
-    go.Bar(name='1A', x=animals, y=[ 149.86777268885785,80.90661410593758, 98.10895740615246,201.05943159974342,41.90069788631746,201.05943159974342,186.69418143435564,194.75466769210482,194.69235075844935]),
-    go.Bar(name='1B', x=animals, y=[ 154.31472688320747,72.3733452271568,  92.813601839818,199.2776749110113,42.19475878906342, 198.05756647597443,98.5361291088928,87.92504386133656, 98.03412212468199])
+    go.Bar(name='2A', x=animals, y=[ 153.38387280921125 ,83.75716800399508, 101.1255316384584,250.28557690493045,19.452932075223554,143.1199839253212,192.52659518771003,250.28557690493045,200.44294598899296]),
+    go.Bar(name='2B', x=animals, y=[ 96.97298864313396,83.05952174561301, 86.53023249040915,139.87105382840673,46.01941123957316,139.87105382840673,104.51361717096081, 115.11970247143795,102.71549292826388])
 ])
 # Change the bar mode
 fig.update_layout(barmode='group')
 fig.update_layout(
-    title_text='Comparison between Goup 1A and 1B',
+    title_text='Comparison between Goup 2A and 2B',
     yaxis_title="timestep",
     font=dict(size=35)  # Adjust the font size as desired
 )
